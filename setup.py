255e86f8dcc748bdecfe8f45fcc550a6550dd2d0
Collier King <collierking99@gmail.com>
2020-11-07 11:49:39 -0600

    2020-11-07

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fusetools",
-    version="0.0.3",
+    version="0.0.2",
     packages=["fusetools"],
     install_requires=["nbsphinx"]
 )
